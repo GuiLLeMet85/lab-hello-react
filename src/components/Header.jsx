c1814a92f5c4a711aa5e4a09b2976efbb18a42a3
Guillem B <guillembaracco@gmail.com>
2022-07-22 01:16:29 +0200

    styling some background images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,17 @@ const Header = () => {
                      the most popular frontend library, <br></br>
                      and become a super Ninja developer</h2>
                     <a href="https://my.ironhack.com/" class="butAw">Awesome</a> 
+
                 </div>
 
                 <div class="image-bkg">
-                        <img src = {biglogo} alt="big logo" class="biglogo"></img>
+                
+                        
                 </div>
                 <div className="image-skg">
-                <img src = {smalllogo} alt="big logo" class="biglogo"></img>
+                <img src = {smalllogo} alt="big logo" class="smalllogo"></img>
+                <img src = {smalllogo} alt="big logo" class="smalllogo"></img>
+                <img src = {smalllogo} alt="big logo" class="smalllogo"></img>
                 </div>
         </header>
     )
